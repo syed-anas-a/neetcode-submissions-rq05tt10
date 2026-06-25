a4d4c777f93ac3dd73a4945ead3d3c0c72ba0bee
Syed Anas Ali <syedanasa86@gmail.com>
2026-06-25 11:29:54 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<Integer> subsets = new ArrayList<>();
+        List<List<Integer>> allSubsets = new ArrayList<>();
+        Arrays.sort(nums);
+        getAllSubsets(nums, 0, subsets, allSubsets);
+        return allSubsets;
+    }
+    public void getAllSubsets(
+            int[] nums, int i, List<Integer> subsets, 
+            List<List<Integer>> allSubsets
+        ) 
+    {
+        if(i == nums.length) {
+            allSubsets.add(new ArrayList<>(subsets));
+            return;
+        }
+        subsets.add(nums[i]);
+        getAllSubsets(nums, i+1, subsets, allSubsets);
+        subsets.remove(subsets.size()-1);
+        int idx = i+1;
+        while(idx < nums.length && nums[idx] == nums[idx-1]) {
+            idx++;
+        }
+        getAllSubsets(nums, idx, subsets, allSubsets);
+    }
+}
